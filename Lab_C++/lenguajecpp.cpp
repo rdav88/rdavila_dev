a1e98d9a062703a5cd45427ac3a4e30ea97c5058
Rubén Alejandro Dávila <rdavila2100@arecibo.inter.edu>
2024-03-04 10:16:03 -0400

    New Files

---
+++ Lab_C++/lenguajecpp.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int myVariable = 10;
+    const int myConstant = 20;
+
+    cout << "Valor de mi variable: " << myVariable << endl;
+    cout << "Valor de mi constante: " << myConstant << endl;
+
+    return 0;
+}
