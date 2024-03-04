91017fd4d19a3fc35a5585f40333193a3ef4734d
Rubén Alejandro Dávila <rdavila2100@arecibo.inter.edu>
2024-03-04 18:17:05 -0400

    New Changes to Java Code

--- Lab_JAVA/LenguajeJava/src/App.java
+++ Lab_JAVA/LenguajeJava/src/App.java
@@ -1,10 +1,9 @@
-public class App 
+class Main
 {
     public static void main(String[] args)
     {
     int myVariable = 10;
     final int myConstant = 20;
-    
     System.out.println("Valor de mi variable: " +myVariable);
     System.out.println("Valor de mi constante: " +myConstant);
     }
