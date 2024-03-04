5765a53f2d7bb4c15be300a448387551580f468e
Rubén Alejandro Dávila <rdavila2100@arecibo.inter.edu>
2024-03-04 18:54:24 -0400

    Archivos Finales

---
+++ Lab_C#/LenguajeCS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+    namespace LenguajeCS
+    class Program {
+        static void Main(){
+            int myVariable = 10;
+            const int myConstant = 20;
+
+            Console.WriteLine("Valor de mi variable: " +myVariable);
+            Console.WriteLine("Valor de mi constante: " +myConstant);
+        }
+    }
--- Lab_JAVA/LenguajeJava/src/App.java
+++ Lab_JAVA/LenguajeJava/src/App.java
@@ -4,6 +4,7 @@ class Main
     {
     int myVariable = 10;
     final int myConstant = 20;
+    
     System.out.println("Valor de mi variable: " +myVariable);
     System.out.println("Valor de mi constante: " +myConstant);
     }
