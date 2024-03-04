89d3294d408125dcb8a4bdb843cb5c7cae7081f3
Rubén Alejandro Dávila <rdavila2100@arecibo.inter.edu>
2024-03-04 14:15:26 -0400

    New Files

---
+++ Lab_JAVA/LenguajeJava/src/App.java
@@ -0,0 +1,11 @@
+public class App 
+{
+    public static void main(String[] args)
+    {
+    int myVariable = 10;
+    final int myConstant = 20;
+    
+    System.out.println("Valor de mi variable: " +myVariable);
+    System.out.println("Valor de mi constante: " +myConstant);
+    }
+}
