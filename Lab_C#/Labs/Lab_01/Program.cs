d00ca57b7f1a94cb820954be863dc0a6e3140cce
Rubén Alejandro Dávila <rdavila2100@arecibo.inter.edu>
2024-03-24 20:56:56 -0400

    Labs en C#

---
+++ Lab_C#/Labs/Lab_01/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        double exam1, exam2, exam3, exam4, promedio;
+        
+        Console.Write("Introduzca el puntaje del Examen 1: ");
+        exam1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Introduzca el puntaje del Examen 2: ");
+        exam2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Introduzca el puntaje del Examen 3: ");
+        exam3 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Introduzca el puntaje del Examen 4: ");
+        exam4 = Convert.ToDouble(Console.ReadLine());
+
+        promedio = (exam1 + exam2 + exam3 + exam4) / 4;
+
+        Console.WriteLine("El promedio del estudiante es " + promedio);
+
+    }
+}
---
+++ Lab_C#/Labs/Lab_02/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        double distancia, velocidad, promedio;
+
+        Console.Write("Introduzca la distancia recorrida en km/h: ");
+        distancia = Convert.ToDouble(Console.ReadLine());
+        
+        Console.Write("Introduzca la velocidad recorrida en horas: ");
+        velocidad = Convert.ToDouble(Console.ReadLine());
+
+        promedio = distancia * velocidad;
+
+        Console.WriteLine("El distancia total recorrida en el viaje es de " + promedio + " Kilometros");
+        
+    }
+}
